bee4b7860fa12c39f10b6475c9bd6daa639cf816
Jyotinder <jyotinder.pec@gmail.com>
2016-02-05 23:43:39 -0700

    21 SVM

--- HOGFeature.py
+++ HOGFeature.py
@@ -5,31 +5,62 @@ import os
 import util
 from config import *
 from sklearn.externals import joblib
+import matplotlib.pyplot as plt
+from skimage import data, color, exposure
+import cv2
 
 # Get the path of the training set
 parser = ap.ArgumentParser()
-parser.add_argument("-t", "--trainingSet", help="Path to Training Set", required="True")
-parser.add_argument("-d", "--debug", help="To make debug print on",type=int, default=1)
+parser.add_argument("-t", "--trainingSet", help="Path to Training Set")
+parser.add_argument("-d", "--debug", help="To make debug print on", type=int, default=1)
+parser.add_argument('-i', "--image", help="Path to the test image",default="")
 args = vars(parser.parse_args())
 
-# Get the training classes names and store them in a list
-train_path = args["trainingSet"]
-training_names = os.listdir(train_path)
-debug = args["debug"]
 
-for dir in training_names:
-    fea_path = os.path.join(pos_feat_ph, dir)
+debug = args["debug"]
+image = args["image"]
+if not image:
+    # Get the training classes names and store them in a list
+    train_path = args["trainingSet"]
+    training_names = os.listdir(train_path)
+    for dir in training_names:
+        fea_path = os.path.join(pos_feat_ph, dir)
+        if debug:
+            print dir
+        if not os.path.isdir(fea_path):
+            os.makedirs(fea_path)
+        set_imgpath = os.path.join(train_path, dir)
+        set_img = os.listdir(set_imgpath)
+        for img in set_img:
+            #im = util.rgb2gray(imread(os.path.join(set_imgpath, img)))
+            im = cv2.imread(os.path.join(set_imgpath, img))
+            im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+            fd = hog(im, orientations, pixels_per_cell, cells_per_block, visualize, normalize)
+            fd_name = os.path.split(img)[1].split(".")[0] + ".feat"
+            fd_path = os.path.join(fea_path, fd_name)
+            joblib.dump(fd, fd_path)
     if debug:
-        print dir
-    if not os.path.isdir(fea_path):
-        os.makedirs(fea_path)
-    set_imgpath = os.path.join(train_path, dir)
-    set_img = os.listdir(set_imgpath)
-    for img in set_img:
-        im = util.rgb2gray(imread(os.path.join(set_imgpath, img)))
-        fd = hog(im, orientations, pixels_per_cell, cells_per_block, visualize, normalize)
-        fd_name = os.path.split(img)[1].split(".")[0] + ".feat"
-        fd_path = os.path.join(fea_path, fd_name)
-        joblib.dump(fd, fd_path)
-if debug:
-    print "Finished Extracting HOG Feature"
+        print "Finished Extracting HOG Feature"
+
+else:
+    im = cv2.imread(image)
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    #fd, hog_image = hog(im, orientations, pixels_per_cell, cells_per_block, visualize, normalize)
+    fd, hog_image = hog(im, orientations=9, pixels_per_cell=(8, 8),
+                        cells_per_block=(3, 3), visualise=True)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+    ax1.axis('off')
+    ax1.imshow(im, cmap=plt.cm.gray)
+    ax1.set_title('Input image')
+    ax1.set_adjustable('box-forced')
+
+    # Rescale histogram for better display
+    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+
+    ax2.axis('off')
+    ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+    ax2.set_title('Histogram of Oriented Gradients')
+    ax1.set_adjustable('box-forced')
+    plt.show()
--- Trainclassifier.py
+++ Trainclassifier.py
@@ -6,34 +6,16 @@ import os
 from config import *
 import numpy as np
 
-if __name__ == "__main__":
-    # Parse the command line arguments
-    parser = ap.ArgumentParser()
-    parser.add_argument('-c', "--classifier", help="Classifier to be used", default="LIN_SVM")
-    parser.add_argument('-fp',"--foldername" , help="folder name for positive feature",  required="True")
-    args = vars(parser.parse_args())
-    # Classifiers supported
-    clf_type = args['classifier']
-    fname = args['foldername']
-    training_names = os.listdir(pos_feat_ph)
-
+def sav_SVM(fname,clf_type,training_names):
     fds = []
     labels = []
     # Load the positive features
+    max=0
     pos_feat_path=os.path.join(pos_feat_ph,fname)
-    count=0
-    prev= ""
     for feat_path in glob.glob(os.path.join(pos_feat_path,"*.feat")):
         fd = joblib.load(feat_path)
-
-        if count == 0 :
-            prev=fd
-            count =1
-        if prev.shape != fd.shape:
-            print feat_path
-            print fd.shape
-            print fd.dtype
-            continue
+        if fd.shape[0]>max:
+            max=fd.shape[0]
 
         fds.append(fd)
         labels.append(1)
@@ -44,25 +26,46 @@ if __name__ == "__main__":
             neg_feat_path=os.path.join(pos_feat_ph,dir)
             for feat_path in glob.glob(os.path.join(neg_feat_path,"*.feat")):
                 fd = joblib.load(feat_path)
-
-                if count == 0 :
-                    prev=fd
-                    count =1
-                if prev.shape != fd.shape:
-                    print feat_path
-                    print fd.shape
-                    print fd.dtype
-                    continue
-
+                if fd.shape[0]>max:
+                    max=fd.shape[0]
                 fds.append(fd)
                 labels.append(0)
+    for index, item in enumerate(fds):
+        if item.shape[0]<max:
+            c=np.zeros(max)
+            c[:item.shape[0]]=item
+            fds[index]=c
 
     if clf_type is "LIN_SVM":
         clf = LinearSVC()
         print "Training a Linear SVM Classifier"
         clf.fit(fds, labels)
         # If feature directories don't exist, create them
-        if not os.path.isdir(os.path.split(model_path)[0]):
-            os.makedirs(os.path.split(model_path)[0])
-        joblib.dump(clf, model_path)
+        path=model_path
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        path=os.path.join(path,fname)
+        if not os.path.isdir(path):
+            os.makedirs(path)
+        path=os.path.join(path,"svm.model")
+        joblib.dump(clf, path)
         print "Classifier saved to {}".format(model_path)
+
+
+if __name__ == "__main__":
+    # Parse the command line arguments
+    parser = ap.ArgumentParser()
+    parser.add_argument('-c', "--classifier", help="Classifier to be used", default="LIN_SVM")
+    parser.add_argument('-fp',"--foldername" , help="folder name for positive feature")
+    parser.add_argument('-all',"--allfoldername" , help="Creating SVM for each of the dataset", type=int, default=0)
+    args = vars(parser.parse_args())
+    # Classifiers supported
+    clf_type = args['classifier']
+    allF = args['allfoldername']
+    training_names = os.listdir(pos_feat_ph)
+    if not allF:
+        fname = args['foldername']
+        sav_SVM(fname,clf_type,training_names)
+    else:
+        for dir in training_names:
+            sav_SVM(dir,clf_type,training_names)
